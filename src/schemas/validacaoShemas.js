fd92496df6161ded6f6eba4ccaae2e0636db73e2
yJeanx11x <j3anlucas010@gmail.com>
2026-03-03 18:27:40 -0300

    feat: adicionar validação de esquema com Zod

---
+++ src/schemas/validacaoShemas.js
@@ -0,0 +1,9 @@
+const z = require('zod')
+
+const validacao = z.object({
+    nome: z.string().min(3, 'Mínimo De 3 caracteres'),
+    email: z.email(),
+    password: z.string().min(6, 'Mínimo De 6 caracteres')
+})
+
+module.exports = validacao

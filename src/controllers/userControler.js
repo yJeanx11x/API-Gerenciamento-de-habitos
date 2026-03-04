f8533d83dfad9718eaa7e302f268c0d2e54156a3
yJeanx11x <j3anlucas010@gmail.com>
2026-03-04 18:47:13 -0300

    feat: implementar função de login e atualizar rotas

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -1,14 +1,16 @@
+require('dotenv').config
 const validacao = require('../schemas/validacaoShemas')
 const db = require('../models/userModels')
-const bcrypt=require('bcrypt')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 
 
 async function criarUsuario(req, res, next) {
     const z = validacao.safeParse(req.body)
-    const passwordHash=await bcrypt.hash(z.data.password,12)
-    
+    const passwordHash = await bcrypt.hash(z.data.password, 12)
+
     try {
         if (!z.success) {
             return res.status(400).json({ message: 'Erro na criação de usuário' })
@@ -18,8 +20,6 @@ async function criarUsuario(req, res, next) {
             email: z.data.email,
             password: passwordHash
         })
-
-
         return res.status(201).json({ message: 'Usuário criado!' })
 
     } catch (error) {
@@ -28,4 +28,40 @@ async function criarUsuario(req, res, next) {
 
 }
 
-module.exports = { criarUsuario }
+async function habitsLogin(req, res, next) {
+    const { email, password } = req.body
+    try {
+        if (!email) {
+            return res.status(400).json({ message: "Email invalido" })
+        }
+        if (!password) {
+            return res.status(400).json({ message: "Senha invalido" })
+        }
+        const usuarioDoDB = await db.findOne({ where: { email } })
+        if (!usuarioDoDB) {
+            return res.status(404).json({ message: 'Usuario não encontrado' })
+        }
+        // comparando a senha
+        const SenhaUsuario = await bcrypt.compare(password, usuarioDoDB.password)
+        if (!SenhaUsuario) {
+            return res.status(404).json({ message: 'Senha incorreta' })
+        }
+
+        try {
+            const secret = process.env.SECRET
+            const token = jwt.sign({
+                id: usuarioDoDB._id
+            }, secret)
+            const userID = usuarioDoDB.id
+            return res.status(200).json({ message: 'Login com sucesso', token, userID })
+        } catch (error) {
+            next(error)
+        }
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+module.exports = { criarUsuario, habitsLogin }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/userControler')
 router.post('/habits', controllers.criarUsuario)
+router.post('/habitsLogin', controllers.habitsLogin)
 
 module.exports = router;
